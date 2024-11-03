7207620de9e2078c9c6713cecd06ba2b8187a2d8
KayoRonald <kayoronald4321@gmail.com>
2024-11-03 12:55:05 +0000

    feat: atualizar página inicial com layout e conteúdo aprimorados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { ColorModeToggle } from "../components/color-mode-toggle";
-import { center } from "../../styled-system/patterns";
+import { center, flex, stack } from "../../styled-system/patterns";
 import { css } from "../../styled-system/css";
 
 function Home() {
@@ -15,12 +15,47 @@ function Home() {
         <ColorModeToggle />
       </div>
       <div
-        className={center({
-         minH: '100vh',
-        })}
-      >
-        Hello World
+      className={css({
+        minH: '100dvh',
+        bg: '#F8FAFC',
+        pt: '96px',
+        px: { base: '24px', lg: '48px' },
+      })}
+    >
+      <div className={flex()}>
+        <div className={stack({ maxW: { lg: '600px' }, mx: 'auto' })}>
+          <h1
+            className={css({
+              fontSize: '36px',
+              fontWeight: '800',
+            })}
+          >
+            Simplified Payments and Invoice Tracking Management
+          </h1>
+          <p
+            className={css({
+              fontSize: '21px',
+            })}
+          >
+            Centralize bill payments and invoice management to help you gain
+            better control over your finances, ensure payments are made on time,
+            and have a clear overview of your financial obligations.
+          </p>
+        </div>
+        <div>
+          <img
+            src='https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmluYW5jZXxlbnwwfHwwfHx8MA%3D%3D'
+            alt='simplified payments and invoices'
+            className={css({
+              width: 'full',
+              height: 'full',
+              borderRadius: '12px',
+              objectFit: 'cover',
+            })}
+          />
+        </div>
       </div>
+    </div>
     </>
   );
 }
